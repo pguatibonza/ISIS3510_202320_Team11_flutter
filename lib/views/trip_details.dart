5d32233b2d7b530cbed56a8371d9ef32f019e2e6
Pablo <p.guatibonza@uniandes.edu.co>
2023-10-30 20:59:03 -0500

    location trip detail
    pickup and dropoff location marked in map

--- lib/views/trip_details.dart
+++ lib/views/trip_details.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:tucamion/models/access_point.dart';
 import 'package:tucamion/models/load.dart';
 import 'package:tucamion/models/trip.dart';
+import 'package:geocoding/geocoding.dart';
 
 class TripDetails extends StatefulWidget {
   const TripDetails({super.key,required this.trip,required this.pickup,required this.dropoff,required this.load});
@@ -15,8 +19,72 @@ class TripDetails extends StatefulWidget {
 }
 
 class _TripDetailsState extends State<TripDetails> {
+  GoogleMapController? _controller;
+  LatLng _pickupLocation = LatLng(0.0, 0.0); // Default position
+  LatLng _dropoffLocation =LatLng(0.0,0.0);
+
+  @override
+  void initState() {
+    super.initState();
+//    requestLocationPermission
+  String pickupAddress="${widget.pickup.address}, ${widget.pickup.city}, ${widget.pickup.country}";
+  String dropoffAddress="${widget.dropoff.address}, ${widget.dropoff.city}, ${widget.dropoff.country}";
+
+    getCoordinatesFromAddress(pickupAddress).then((coordinates) {
+      setState(() {
+        _pickupLocation = coordinates;
+      });
+    });
+    getCoordinatesFromAddress(dropoffAddress).then((coordinates) {
+      setState(() {
+        _dropoffLocation = coordinates;
+      });
+    });
+
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: GoogleMap(
+        mapType: MapType.normal, // Set the map type
+        initialCameraPosition: CameraPosition(target: _pickupLocation, zoom: 15),
+        markers: {
+          Marker(
+            markerId: MarkerId('Pickup'),
+            position: _pickupLocation,
+            infoWindow: InfoWindow(title: 'Address Marker'),
+          ),
+          Marker(
+            markerId: MarkerId('Dropoff'),
+            position: _dropoffLocation,
+            infoWindow: InfoWindow(title: 'Address Marker'),
+          ),
+        },
+      ),
+
+      
+    );
   }
+}
+
+
+Future<LatLng> getCoordinatesFromAddress(String address) async {
+  bool isLocationServiceEnabled  = await Geolocator.isLocationServiceEnabled();
+  print(address);
+  try{
+  List<Location> locations = await locationFromAddress(address);
+  if (locations.isNotEmpty) {
+    final location = locations[0];
+    print("prrrrrrrrrrrrrrrrrr");
+    print(location.latitude);
+    return LatLng(location.latitude, location.longitude);
+  }
+  }
+  catch(e){
+    print("errrorr $e");
+  }
+  
+  return LatLng(0.0, 0.0); // Default position if address not found
 }
