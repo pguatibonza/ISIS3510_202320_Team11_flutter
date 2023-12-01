9eac31845980e91ccb2271d4f4fc59d51a635728
Pablo <p.guatibonza@uniandes.edu.co>
2023-12-01 15:24:06 -0500

    asign trailer to trip

--- lib/views/trip_details.dart
+++ lib/views/trip_details.dart
@@ -3,9 +3,11 @@ import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:tucamion/controller/api.dart';
 import 'package:tucamion/models/access_point.dart';
 import 'package:tucamion/models/load.dart';
 import 'package:tucamion/models/trip.dart';
+import 'package:http/http.dart' as http;
 
 class TripDetails extends StatefulWidget {
   const TripDetails({super.key,required this.trip,required this.pickup,required this.dropoff,required this.load});
@@ -52,10 +54,13 @@ class _TripDetailsState extends State<TripDetails> {
 
     return Scaffold(
   appBar: AppBar(title:Text("Trip Info"),),
-  body: Stack(
+  body: Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.stretch,
     children: [
       Center(
         child: Container(
+          height:200,
           margin: EdgeInsets.all(8),
           child: GoogleMap(
             mapType: MapType.normal,
@@ -144,7 +149,7 @@ class _TripDetailsState extends State<TripDetails> {
                                           .bodyMedium
                                           ?.color))
                 else
-                    Text('We have found a truck a truck available',style: GoogleFonts.montserrat(
+                    Text('We have found a truck available',style: GoogleFonts.montserrat(
                                       fontSize: 14 * ffem,
                                       fontWeight: FontWeight.w600,
                                       height: 1.2000000477 * ffem / fem,
@@ -157,7 +162,28 @@ class _TripDetailsState extends State<TripDetails> {
           ),
         ],
       ),
-    ],
+    SizedBox(height: 25*ffem,),
+    widget.trip.trailer==null?
+    ElevatedButton(
+      onPressed: (){
+        assignTripToTrailer(widget.trip.id,context);
+      },
+      style: ElevatedButton.styleFrom(
+                    primary: Colors.blue, // Background color
+                    onPrimary: Colors.white, // Text color
+                    padding: EdgeInsets.all(16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: Text(
+                    'Asignar camion',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 16 * ffem,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white, // Text color
+                    ),
+    )): SizedBox()],
   ),
 );
 
@@ -181,4 +207,77 @@ Future<LatLng> getCoordinatesFromAddress(String address) async {
   }
   
   return LatLng(0.0, 0.0); // Default position if address not found
+}
+Future<void> assignTripToTrailer(int tripId,context) async {
+  final String apiUrl = '${api}trips/assignTrailer/$tripId';
+
+  try {
+    final response = await http.get(
+      Uri.parse(apiUrl),
+    );
+
+    if (response.statusCode == 200) {
+      print('Trip assigned successfully');
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Success'),
+            content: Text('Trip assigned to a trailer'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      print('Failed to assign trip: ${response.statusCode}');
+      print('Response body: ${response.body}');
+    }
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Failed to Assign Trip'),
+            content: Text(response.body),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+  } catch (e) {
+    print('Error during request: $e');
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Error'),
+          content: Text('Error during request: $e'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+
+    
+  }
 }
