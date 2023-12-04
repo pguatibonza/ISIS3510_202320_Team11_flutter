35e90f7bc8b55d806c1b769db28b15db0e985b37
Juan David Díaz <juanddiaz13@gmail.com>
2023-12-04 16:44:06 -0500

    updated current trip

--- lib/views/current_trip.dart
+++ lib/views/current_trip.dart
@@ -11,6 +11,8 @@ import 'package:tucamion/models/load.dart';
 import 'package:tucamion/models/trailer.dart';
 import 'package:tucamion/models/trip.dart';
 import 'package:http/http.dart' as http;
+import 'package:tucamion/controller/connectivityController.dart';
+import 'package:tucamion/views/CustomAlertDialog.dart';
 
 class CurrentTrip extends StatefulWidget {
   const CurrentTrip({super.key});
@@ -24,10 +26,10 @@ class _CurrentTripState extends State<CurrentTrip> {
   LatLng _dropoffLocation = LatLng(0.0, 0.0);
   LatLng _bogota = LatLng(4.624335, -74.063644);
   Trailer? trailer;
-  late Trip tripsito;
-  late AccessPoint pickup;
-  late AccessPoint dropoff;
-  late Load load;
+  Trip? tripsito;
+  AccessPoint? pickup;
+  AccessPoint? dropoff;
+  Load? load;
 
   @override
   void initState() {
@@ -53,179 +55,207 @@ class _CurrentTripState extends State<CurrentTrip> {
     }
 
     print("Retrieving data ");
-    final response2 =
-        await http.get(Uri.parse('$TRIPSDRIVER/${trailer!.id}/status/IP'));
-    var data2 = json.decode(response2.body)[0];
-    print(data2);
+    try {
+      final response2 =
+          await http.get(Uri.parse('$TRIPSDRIVER/${trailer!.id}/status/IP'));
+      if (response2.statusCode == 200) {
+        var data2 = json.decode(response2.body)[0];
 
-    tripsito = Trip.fromJson(data2);
+        print(data2);
 
-    final pickupResponse =
-        await http.get(Uri.parse('$accessPoints/${tripsito.pickup}'));
-    final dataAccess = json.decode(pickupResponse.body);
+        tripsito = Trip.fromJson(data2);
 
-    pickup = AccessPoint.fromJson(dataAccess);
+        final pickupResponse =
+            await http.get(Uri.parse('$accessPoints/${tripsito?.pickup}'));
+        final dataAccess = json.decode(pickupResponse.body);
 
-    final dropoffResponse =
-        await http.get(Uri.parse('$accessPoints/${tripsito.dropoff}'));
-    final dataDropoff = json.decode(dropoffResponse.body);
+        pickup = AccessPoint.fromJson(dataAccess);
 
-    dropoff = AccessPoint.fromJson(dataDropoff);
+        final dropoffResponse =
+            await http.get(Uri.parse('$accessPoints/${tripsito?.dropoff}'));
+        final dataDropoff = json.decode(dropoffResponse.body);
 
-    final loadResponse = await http.get(Uri.parse('$loads/${tripsito.load}'));
-    final dataLoad = json.decode(loadResponse.body);
+        dropoff = AccessPoint.fromJson(dataDropoff);
 
-    load = Load.fromJson(dataLoad);
+        final loadResponse =
+            await http.get(Uri.parse('$loads/${tripsito?.load}'));
+        final dataLoad = json.decode(loadResponse.body);
 
-    String pickupAddress =
-        "${pickup.address}, ${pickup.city}, ${pickup.country}";
-    String dropoffAddress =
-        "${dropoff.address}, ${dropoff.city}, ${dropoff.country}";
+        load = Load.fromJson(dataLoad);
 
-    await getCoordinatesFromAddress(pickupAddress).then((coordinates) {
-      setState(() {
-        _pickupLocation = coordinates;
-      });
-    });
-    await getCoordinatesFromAddress(dropoffAddress).then((coordinates) {
-      setState(() {
-        _dropoffLocation = coordinates;
-      });
-    });
+        String pickupAddress =
+            "${pickup?.address}, ${pickup?.city}, ${pickup?.country}";
+        String dropoffAddress =
+            "${dropoff?.address}, ${dropoff?.city}, ${dropoff?.country}";
+
+        await getCoordinatesFromAddress(pickupAddress).then((coordinates) {
+          setState(() {
+            _pickupLocation = coordinates;
+          });
+        });
+        await getCoordinatesFromAddress(dropoffAddress).then((coordinates) {
+          setState(() {
+            _dropoffLocation = coordinates;
+          });
+        });
+      } else {
+        print("No hay viaje actual");
+      }
+    } catch (e) {
+      print('Error $e');
+    }
   }
 
   @override
-Widget build(BuildContext context) {
-  double baseWidth = 360;
-  double fem = MediaQuery.of(context).size.width / baseWidth;
-  double ffem = fem * 0.97;
+  Widget build(BuildContext context) {
+    double baseWidth = 360;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    double ffem = fem * 0.97;
 
-  return Scaffold(
-    appBar: AppBar(
-      title: Text("Trip Info"),
-    ),
-    body: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        
-        Center(
-          child: Container(
-            height: 200, // Set the desired height for the map
-            margin: EdgeInsets.all(8),
-            child: GoogleMap(
-              mapType: MapType.normal,
-              initialCameraPosition: CameraPosition(target: _bogota, zoom: 5),
-              markers: {
-                Marker(
-                  markerId: MarkerId('Pickup'),
-                  position: _pickupLocation,
-                  infoWindow: InfoWindow(title: 'Pickup Address'),
-                ),
-                Marker(
-                  markerId: MarkerId('Dropoff'),
-                  position: _dropoffLocation,
-                  infoWindow: InfoWindow(title: 'Dropoff Address'),
-                ),
-              },
-            ),
-          ),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              padding: EdgeInsets.all(16),
-              color: const Color.fromARGB(255, 0, 0, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Load Type: ${load.type}',
-                        style: GoogleFonts.montserrat(
-                            fontSize: 14 * ffem,
-                            fontWeight: FontWeight.w600,
-                            height: 1.2000000477 * ffem / fem,
-                            color:
-                                Theme.of(context).textTheme.bodyMedium?.color)),
-                    Text('Trailer Type: ${load.trailerType}',
-                        style: GoogleFonts.montserrat(
-                            fontSize: 14 * ffem,
-                            fontWeight: FontWeight.w600,
-                            height: 1.2000000477 * ffem / fem,
-                            color:
-                                Theme.of(context).textTheme.bodyMedium?.color)),
-                    Text('Weight: ${load.weight} lbs',
-                        style: GoogleFonts.montserrat(
-                            fontSize: 14 * ffem,
-                            fontWeight: FontWeight.w600,
-                            height: 1.2000000477 * ffem / fem,
-                            color:
-                                Theme.of(context).textTheme.bodyMedium?.color)),
-                    if (load.volume != null)
-                      Text('Volume: ${load.volume} cubic ft',
-                          style: GoogleFonts.montserrat(
-                              fontSize: 14 * ffem,
-                              fontWeight: FontWeight.w600,
-                              height: 1.2000000477 * ffem / fem,
-                              color: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium
-                                  ?.color)),
-                    Text(
-                        'Pickup Address: ${pickup.address}, ${pickup.city}, ${pickup.country}',
-                        style: GoogleFonts.montserrat(
-                            fontSize: 14 * ffem,
-                            fontWeight: FontWeight.w600,
-                            height: 1.2000000477 * ffem / fem,
-                            color:
-                                Theme.of(context).textTheme.bodyMedium?.color)),
-                    Text(
-                        'Dropoff Address: ${dropoff.address}, ${dropoff.city}, ${dropoff.country}',
-                        style: GoogleFonts.montserrat(
-                            fontSize: 14 * ffem,
-                            fontWeight: FontWeight.w600,
-                            height: 1.2000000477 * ffem / fem,
-                            color:
-                                Theme.of(context).textTheme.bodyMedium?.color)),
-                ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Trip Info"),
+      ),
+      body: tripsito == null
+          ? const Center(
+              // Display a message when there is no current trip
+              child: Text(
+                'No current trip',
+                style: TextStyle(fontSize: 16),
               ),
+            )
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Center(
+                  child: Container(
+                    height: 200, // Set the desired height for the map
+                    margin: EdgeInsets.all(8),
+                    child: GoogleMap(
+                      mapType: MapType.normal,
+                      initialCameraPosition:
+                          CameraPosition(target: _bogota, zoom: 5),
+                      markers: {
+                        Marker(
+                          markerId: MarkerId('Pickup'),
+                          position: _pickupLocation,
+                          infoWindow: InfoWindow(title: 'Pickup Address'),
+                        ),
+                        Marker(
+                          markerId: MarkerId('Dropoff'),
+                          position: _dropoffLocation,
+                          infoWindow: InfoWindow(title: 'Dropoff Address'),
+                        ),
+                      },
+                    ),
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(16),
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Load Type: ${load?.type}',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 14 * ffem,
+                                  fontWeight: FontWeight.w600,
+                                  height: 1.2000000477 * ffem / fem,
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyMedium
+                                      ?.color)),
+                          Text('Trailer Type: ${load?.trailerType ?? "N/A"}',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 14 * ffem,
+                                  fontWeight: FontWeight.w600,
+                                  height: 1.2000000477 * ffem / fem,
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyMedium
+                                      ?.color)),
+                          Text('Weight: ${load?.weight ?? "N/A"} lbs',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 14 * ffem,
+                                  fontWeight: FontWeight.w600,
+                                  height: 1.2000000477 * ffem / fem,
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyMedium
+                                      ?.color)),
+                          if (load?.volume != null)
+                            Text('Volume: ${load?.volume} cubic ft',
+                                style: GoogleFonts.montserrat(
+                                    fontSize: 14 * ffem,
+                                    fontWeight: FontWeight.w600,
+                                    height: 1.2000000477 * ffem / fem,
+                                    color: Theme.of(context)
+                                        .textTheme
+                                        .bodyMedium
+                                        ?.color)),
+                          Text(
+                              'Pickup Address: ${pickup?.address}, ${pickup?.city}, ${pickup?.country}',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 14 * ffem,
+                                  fontWeight: FontWeight.w600,
+                                  height: 1.2000000477 * ffem / fem,
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyMedium
+                                      ?.color)),
+                          Text(
+                              'Dropoff Address: ${dropoff?.address}, ${dropoff?.city}, ${dropoff?.country}',
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 14 * ffem,
+                                  fontWeight: FontWeight.w600,
+                                  height: 1.2000000477 * ffem / fem,
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyMedium
+                                      ?.color)),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 25 * ffem),
+                ElevatedButton(
+                  onPressed: () {
+                    if (ConnectivityController.hasInternet) {
+                      updateTripStatus(tripsito!.id);
+                    } else {
+                      CustomAlertDialog.showAlertDialog(context);
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue, // Background color
+                    onPrimary: Colors.white, // Text color
+                    padding: EdgeInsets.all(16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: Text(
+                    'Terminar Envio',
+                    style: GoogleFonts.montserrat(
+                      fontSize: 16 * ffem,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white, // Text color
+                    ),
+                  ),
+                )
+              ],
             ),
-          ],
-        ),ElevatedButton(
-  onPressed: () {
-    // Your button action here
-  },
-  style: ElevatedButton.styleFrom(
-    primary: Colors.blue, // Background color
-    onPrimary: Colors.white, // Text color
-    padding: EdgeInsets.all(16),
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(10.0),
-    ),
-  ),
-  child: Text(
-    'Terminar Envio',
-    style: GoogleFonts.montserrat(
-      fontSize: 16 * ffem,
-      fontWeight: FontWeight.w600,
-      color: Colors.white, // Text color
-    ),
-  ),
-)
-
-
-      ],
-    ),
-  );
-}
-
-
- 
+    );
+  }
 }
 
 Future<LatLng> getCoordinatesFromAddress(String address) async {
-
   print(address);
   try {
     List<Location> locations = await locationFromAddress(address);
@@ -239,4 +269,29 @@ Future<LatLng> getCoordinatesFromAddress(String address) async {
   }
 
   return LatLng(0.0, 0.0); // Default position if address not found
+}
+
+// Función para actualizar el estado del viaje
+Future<void> updateTripStatus(int id) async {
+  try {
+    // Realiza una solicitud PUT o POST a tu endpoint de actualización de estado
+    final response = await http.put(
+      Uri.parse('$trips/updateStatus/$id'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    // Verifica si la solicitud fue exitosa (código de estado 200)
+    if (response.statusCode == 200) {
+      // Muestra un mensaje o realiza otras acciones si es necesario
+      print('Estado del viaje actualizado con éxito');
+    } else {
+      // Maneja el caso en el que la solicitud no fue exitosa
+      print('Error al actualizar el estado del viaje: ${response.body}');
+    }
+  } catch (e) {
+    // Maneja errores de red u otros errores
+    print('Error al realizar la solicitud: $e');
+  }
 }
